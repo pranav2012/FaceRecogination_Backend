f5b4e36cf516975f7deef9781568a896e4854be3
Pranav Agarwal <p2012agarwal@gmail.com>
2020-07-19 19:12:22 +0530

    adding database

--- database/database.js
+++ database/database.js
@@ -1,13 +1,13 @@
 const knex = require('knex');
 
 const db = knex({
-    client: 'pg',
-    connection: {
-      host : '127.0.0.1',
-      user : 'postgres',
-      password : 'mynameisp',
-      database : 'facerec'
+  client: 'pg',
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
     }
-  });
+  }
+});
 
 module.exports = db;
